c96fb582be69535625b8570da58f185aa310bb79
keofc <keoitc2014@gmail.com>
2026-05-14 09:52:16 +0700

    ddd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.62"],
+  allowedDevOrigins: [
+    "10.0.40.9",
+    "192.168.1.62",
+    "localhost",
+    "127.0.0.1",
+
+    // ถ้ามี domain ภายใน
+    "*.odienmall.com",
+  ],
 };
 
 export default nextConfig;
